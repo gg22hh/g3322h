ecd54db661188303fe4d8a93c27372deba224673
gg22hh <g2203h@gmail.com>
2021-09-25 16:13:25 +0300

    added main content

---
+++ #src/js/script.js
@@ -0,0 +1,9 @@
+new Swiper(".image-slider", {
+    navigation: {
+        nextEl: ".button-next",
+        prevEl: ".button-prev",
+    },
+	direction: 'vertical',
+	slidesPerView: 2,
+	spaceBetween: 7,
+});
