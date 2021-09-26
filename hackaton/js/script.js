27e632666b8c5755fa550bd4b4fe6a389c6db39f
gg22hh <g2203h@gmail.com>
2021-09-26 03:53:45 +0300

    fixed homepage

--- hackaton/js/script.js
+++
@@ -1,9 +0,0 @@
-new Swiper(".image-slider", {
-    navigation: {
-        nextEl: ".button-next",
-        prevEl: ".button-prev",
-    },
-	direction: 'vertical',
-	slidesPerView: 2,
-	spaceBetween: 7,
-});
